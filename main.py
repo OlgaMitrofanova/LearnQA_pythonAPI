6072e53e306003998b95262d5895e07ba325e25a
Olga <chainikova@gmail.com>
2024-01-17 21:23:33 +0300

    get_txt

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ print("Hello from Olga")
 
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
